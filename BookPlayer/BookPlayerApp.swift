e6003e878bed0f506d734bad25a4deb6ef6399f0
VitaliyD <dergunov.vitaly@gmail.com>
2024-07-17 14:07:36 +0300

    Initial Commit

---
+++ BookPlayer/BookPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPlayerApp.swift
+//  BookPlayer
+//
+//  Created by Vitaliy on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
